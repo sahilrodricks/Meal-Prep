1a7fedcbf8a805cf4d6b71225c83e0a73f98ce95
lmiro98 <l.miro98@gmail.com>
2017-04-25 12:31:05 -0400

    Add files via upload

---
+++ TrackerController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MealPrep
+//
+//  Created by Student on 4/24/17.
+//  Copyright © 2017 shp. All rights reserved.
+//
+
+import UIKit
+
+class TrackerController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var button: UIButton!
+    
+}
+
